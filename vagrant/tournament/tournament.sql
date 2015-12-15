d9671ba8c5225a66b7d28e785dfec097140ac9f1
Ahmad Luqman <ahmad.luqman@gmail.com>
2015-12-15 20:56:25 +0500

    Clean the scripts to also create and delete databases other than tables and views

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,10 +1,7 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+--This script creates a database and create all the tables 
+--and views required for the tournament.
+CREATE DATABASE tournament;
+\c tournament;
 
 CREATE TABLE IF NOT EXISTS players ( id SERIAL PRIMARY KEY,
 					   name TEXT );
--- vagrant/tournament/tournamentclean.sql
+++ vagrant/tournament/tournamentclean.sql
@@ -1,5 +1,11 @@
+--This script deletes all views and tables and also 
+--deletes the database. Use with caution and remember
+--to backup data before executing this
+\c tournament
 DROP VIEW IF EXISTS players_standings;
 DROP VIEW IF EXISTS players_wins;
 DROP VIEW IF EXISTS players_matches_played;
 DROP TABLE IF EXISTS matches;
 DROP TABLE IF EXISTS players;
+\c vagrant
+DROP DATABASE IF EXISTS tournament;
